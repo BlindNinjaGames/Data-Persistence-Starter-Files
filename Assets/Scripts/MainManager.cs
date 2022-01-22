4f530dce58f4beeb63ab25b022eeea17f6e913ea
BlindNinjaGames <36018103+BlindNinjaGames@users.noreply.github.com>
2022-01-22 13:12:29 -0600

    Data Persistence
    Project completed.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,62 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using TMPro;
+
 
 public class MainManager : MonoBehaviour
 {
-    public Brick BrickPrefab;
-    public int LineCount = 6;
-    public Rigidbody Ball;
+    //ADD THIS SCRIPT TO AN EMPTY GAME OBJECT CALLED "MainManager" ON THE GAMEPLAY SCREEN.
 
-    public Text ScoreText;
-    public GameObject GameOverText;
-    
-    private bool m_Started = false;
-    private int m_Points;
-    
-    private bool m_GameOver = false;
+    public Brick brickPrefab;
+    public int lineCount = 6;
+    public Rigidbody ball;
 
-    
-    // Start is called before the first frame update
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI gameOverText;
+    [SerializeField] TextMeshProUGUI highScoreText;
+    [SerializeField] TextMeshProUGUI firstTimeText;
+
+    GameManager gameManager;
+
+
+    bool isGameStarted = false;
+    int point;
+
+    bool isGameOver = false;
+
+
+
     void Start()
     {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+        for (int i = 0; i < lineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
             {
                 Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                var brick = Instantiate(brickPrefab, position, Quaternion.identity);
                 brick.PointValue = pointCountArray[i];
                 brick.onDestroyed.AddListener(AddPoint);
             }
         }
+
+        HighScoreText();  //This will update the high score text whenever the scene loads.
     }
 
     private void Update()
     {
-        if (!m_Started)
+        if (!isGameStarted)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                m_Started = true;
+
+                isGameStarted = true;
                 float randomDirection = Random.Range(-1.0f, 1.0f);
                 Vector3 forceDir = new Vector3(randomDirection, 1, 0);
                 forceDir.Normalize();
 
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
+                ball.transform.SetParent(null);
+                ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
             }
         }
-        else if (m_GameOver)
+        else if (isGameOver)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
+                gameManager.congratulationsText.gameObject.SetActive(false);  //Turn off the player name input field.
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
@@ -64,13 +77,61 @@ public class MainManager : MonoBehaviour
 
     void AddPoint(int point)
     {
-        m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
+        this.point += point;
+        scoreText.text = $"Score : {this.point}";
     }
 
     public void GameOver()
     {
-        m_GameOver = true;
-        GameOverText.SetActive(true);
+        isGameOver = true;
+        gameOverText.gameObject.SetActive(true);
+
+        //When the game ends, check to see which number is higher. If the score is higher than the currently saved high score
+        //then activate the player name input field. Also, change the high score to the point score.
+
+        if (point > gameManager.playerHighScore)
+        {
+            gameManager.congratulationsText.gameObject.SetActive(true);
+            gameManager.playerHighScore = point;
+        }
     }
+
+    //This changes the high score text at the top of the screen based on whether a save file exists or not which is
+    //controlled by the boolean "isFirstTimePlaying". I used separate text boxes so the messages would line up correctly.
+    public void HighScoreText()
+    {
+        if (gameManager.isFirstTimePlaying == false)
+        {
+            highScoreText.gameObject.SetActive(true);
+            firstTimeText.gameObject.SetActive(false);
+        }
+
+        else
+        {
+            highScoreText.gameObject.SetActive(false);
+            firstTimeText.gameObject.SetActive(true);
+        }
+
+        if (gameManager.isFirstTimePlaying == false)
+        {
+            highScoreText.text = gameManager.playerName + " has the high score of: " + gameManager.playerHighScore;
+        }
+
+        else
+        {
+            firstTimeText.text = "Show them how it's done!";
+        }
+
+    }
+
+    //Add this to a main menu button. 
+    public void MainMenu()
+    {
+        gameManager.congratulationsText.gameObject.SetActive(false);  //turns off the player input field.
+        gameManager.deleteSaveDataButton.gameObject.SetActive(true);  //brings the Delete Save button back.
+
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+
 }
--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -1,18 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+//You need this so the code so the Exit button will work.
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+//THIS SCRIPT SHOULD GO ON THE CANVAS ON THE TITLE SCREEN THAT HOLDS THE TITLE, SAVE / QUIT BUTTONS, AND OPENING MESSAGE TEXT
 
 public class MenuHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    GameManager gameManager;
+
+    [SerializeField] TextMeshProUGUI openingMessage;
+
+
+    private void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        //Change the opening message at the bottom of the screen depnding on whether there save data or not.
+        //This was easiest to control using a boolean. 
+
+        if (gameManager.isFirstTimePlaying == false)
+        {
+            openingMessage.text = gameManager.playerName + " has the high score of: " + gameManager.playerHighScore;
+
+        }
+
+        else
+        {
+            openingMessage.text = "You are the first to play!\nNow is the time to set the standard!";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartNew()
     {
-        
+        gameManager.deleteSaveDataButton.gameObject.SetActive(false);  //This just hides the delete button when playing the game.
+
+        SceneManager.LoadScene("main");  //Make sure the name of the scene matches EXACTLY
     }
+
+    public void Exit()
+    {
+        //Use this to be able to quit the game while in Unity.
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+
+        Application.Quit();
+#endif
+    }
+
+
+
 }
